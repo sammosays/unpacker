ee6b3c7e01b29ab2838312564779830052d48c2f
Sam <hidden@hidden.com>
2023-02-02 20:30:57 +0000

    fix attempt

--- unpacker.py
+++ unpacker.py
@@ -43,7 +43,7 @@ def callback_download(ch, method, properties, body):
             bucket = record['s3']['bucket']['name']
             key = record['s3']['object']['key']
             global s3
-            obj = s3.get_object(Bucket=bucket, Key=key)
+            obj = s3.get_object(Bucket=bucket, Key=key)['Body'].read()
             print(f'{obj}')
 
 
