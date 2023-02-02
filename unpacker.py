964de06f3989f412e551aadd8149863448077272
Sam <hidden@hidden.com>
2023-02-02 21:31:23 +0000

    post each file to minio

--- unpacker.py
+++ unpacker.py
@@ -9,11 +9,13 @@ import os
 import sys
 from zipfile import ZipFile
 
+
 MINIO_TOKEN_ENV_VAR = 'MINIO_ACCESS_TOKEN'
 MINIO_SECRET_ENV_VAR = 'MINIO_ACCESS_SECRET'
 MINIO_URL = 'http://minio-service:9000'
 NOTIFY_RECEIVE_QUEUE = 'unpacker-queue'
 NOTIFY_SEND_QUEUE = ''
+UNPACKED_BUCKET = 'unpacked'
 
 
 def load_env_var(name):
@@ -37,22 +39,46 @@ s3 = boto3.client(
 )
 
 
-def unzip(zip_archive, file_name):
-    zip_file = ZipFile(io.BytesIO(zip_archive['Body'].read()))
-    file = zip_file.open(file_name).read()
-    print(f'{file}')
+def callback(ch, method, properties, body):
+    global s3
+    global UNPACKED_BUCKET
 
-
-def callback_download(ch, method, properties, body):
+    # receive event from rabbit
     print(f'received: {body}')
     event = json.loads(body.decode())
+    records = []
+
+    # process each record in the event
     if 'EventName' in event and event['EventName'] == 's3:ObjectCreated:Put':
         for record in event['Records']:
             bucket = record['s3']['bucket']['name']
             key = record['s3']['object']['key']
-            global s3
-            obj = s3.get_object(Bucket=bucket, Key=key)
-            unzip(obj, key)
+
+            try:
+                # download object as a zip
+                obj = s3.get_object(Bucket=bucket, Key=key)
+                zip_file = ZipFile(io.BytesIO(obj['Body'].read()))
+
+                # unzip each file in memory
+                unpacked = []
+                for file_name in zip_file.namelist():
+                    file = zip_file.open(file_name).read()
+
+                    # post file to minio and add to record
+                    s3.put_object(Body=file, Bucket=UNPACKED_BUCKET, Key=file_name)
+                    unpacked.append({'key': file_name, 'bucket': UNPACKED_BUCKET})
+
+                # update event
+                record['unpacked'] = unpacked
+                records.append(record)
+
+            except Exception as e:
+                print(f'error processing key [{key}] from bucket [{bucket}] - {e}')
+
+        # post event to rabbit
+        event['Records'] = records
+
+
 
 
 
@@ -63,7 +89,7 @@ def main():
     channel.queue_declare(queue=NOTIFY_RECEIVE_QUEUE)
     channel.basic_consume(queue=NOTIFY_RECEIVE_QUEUE,
                           auto_ack=True,
-                          on_message_callback=callback_download)
+                          on_message_callback=callback)
 
     print('listening for notifications..')
     channel.start_consuming()
